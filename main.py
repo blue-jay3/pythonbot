f20c697c673999ca8a1715514cd6f6c361d81301
blue-jay3 <emferra01@gmail.com>
2024-07-03 23:15:05 -0400

    sends a dm to the user with the bot's latency on the specified schedule, added command to validate website url

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import discord # from pip install py-cord
 import os # default module
 from dotenv import load_dotenv # from pip install python-dotenv
 
+import schedule
+import time
+from discord.ext import tasks
+import validators
+
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot(intents=discord.Intents.all()) # create a new instance of discord.Bot()
 
@@ -9,33 +14,26 @@ bot = discord.Bot(intents=discord.Intents.all()) # create a new instance of disc
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-# slash commands
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Hey!")
+# notification functions
+@tasks.loop(minutes=60)
+async def notify_user():
+    await bot.wait_until_ready()
+    user = bot.get_user(396050286420164610)
+    # requests.get("http://127.0.0.1").elapsed.total_seconds()
+    await user.send(f"Latency is {round(bot.latency * 1000)}ms.")
 
+notify_user.start()
+
+# slash commands
 @bot.slash_command(name="ping", description="Sends the bot's latency.")
 async def ping(ctx: discord.ApplicationContext):
     await ctx.respond(f"Pong! Latency is {round(bot.latency * 1000)}ms.")
 
-@bot.slash_command(name="game", description="Play a number guessing game")
-async def gtn(ctx: discord.ApplicationContext):
-    embed = discord.Embed( # embed
-        title="Number Guessing Game",
-        description="Guess a number between 1 and 10.",
-        color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
-    )
-    embed.set_footer(text="Type \'Cancel\' to end the game.")
-    await ctx.respond(embed=embed)
-    while True:
-        guess = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
-        if guess.content.lower() == 'cancel':
-            await ctx.send('You cancelled the game.')
-            break
-        elif int(guess.content) == 5:
-            await ctx.send('You guessed it!')
-            break
-        else:
-            await ctx.send('Nope, try again or type \'Cancel\'.')
+@bot.slash_command(name="add-website", description="Add a website to get notifications for it")
+async def gtn(ctx: discord.ApplicationContext, url):
+    if validators.url(url):
+        await ctx.respond("You will now get notified for the site " + url)
+    else:
+        await ctx.respond("Invalid URL")
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
